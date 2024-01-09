997aae453dc80538821f2bd0e47909feaef617f5
CasparKTK <caspar246@gmail.com>
2024-01-09 19:16:44 +0100

    2 functions added

--- backend/backend/assistant/first_contact.py
+++ backend/backend/assistant/first_contact.py
@@ -6,6 +6,31 @@ from .chatgpt import SyncedGPT, SyncedHistory
 class LegalDBToolkit(Toolkit):
     def __init__(self):
         super().__init__()
+        self.summary = {}
+        self.chatEnd = False
+
+        @function_tool(name = "summarize_first_contact")
+        async def summarize_first_contact(self, name:str, email:str, case: str ):
+            '''
+            Summarize potential client's contact and case details for the lawyer and ask for potential's client permission to send 
+
+            Args:
+                name: name of the client
+                email: email of the client
+                case: summary of the legal situation of the client
+
+            '''
+            self.summary["name"] = name
+            self.summary["email"] = email
+            self.summary["case"] = case
+        
+        @function_tool(name = "end_chat")
+        async def end_chat(self):
+            '''
+            Terminate chat
+            '''
+            self.chatEnd = True
+
 
 
 class FirstContactBot(SyncedGPT):
@@ -16,6 +41,9 @@ class FirstContactBot(SyncedGPT):
 
         super().__init__(
             messages=initial_messages,
-            # tools=LegalDBToolkit(),
+            tools=LegalDBToolkit(),
         )
+
+
+
 
