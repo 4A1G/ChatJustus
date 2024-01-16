e5c6d590acbaad48c4c0792c0d65fa3153393562
CasparKTK <caspar246@gmail.com>
2024-01-16 21:01:07 +0100

    util update

--- backend/backend/assistant/util.py
+++ backend/backend/assistant/util.py
@@ -1,2 +1,5 @@
 def format_query_result(query_result):
-    return "\n...\n".join(f"{d.speaker}: {d.content}" for d in query_result if d.speaker == "Justicius")
+    return "\n...\n".join(f"{d.speaker}: {d.content}" for d in query_result)
+
+def format_query_result_speaker(query_result, speaker):
+    return "\n...\n".join(f"{d.speaker}: {d.content}" for d in query_result if d.speaker == speaker)
