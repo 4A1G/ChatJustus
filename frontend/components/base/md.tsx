dfbb3e1a367896c1cdf3be28c87e0d57cd5067fd
Joong-Won Seo <joong.won.seo@gmail.com>
2024-01-20 00:40:36 +0100

    citation sources formatting

--- frontend/components/base/md.tsx
+++ frontend/components/base/md.tsx
@@ -4,6 +4,7 @@ import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import rehypeKatex from 'rehype-katex'
 import remarkMath from 'remark-math'
+import rehypeRaw from 'rehype-raw'
 import 'katex/dist/katex.min.css' // `rehype-katex` does not import the CSS for you
 
 
@@ -14,10 +15,10 @@ interface MDProps {
 }
 
 const MD = ({ className, children, ...props }: MDProps) => (
-  <div className={`overflow-auto prose dark:prose-invert prose-neutral max-w-none prose-pre:max-w-full prose-code:before:hidden prose-code:after:hidden ${className ?? ''}`}>
+  <div className={`overflow-auto prose dark:prose-invert prose-neutral max-w-none prose-pre:max-w-full prose-code:before:hidden prose-code:after:hidden prose-hr:m-1 prose-hr:border-primary/100 prose-a:text-primary/100 ${className ?? ''}`}>
     <Markdown
       remarkPlugins={[remarkGfm, remarkMath]}
-      rehypePlugins={[rehypeKatex]}
+      rehypePlugins={[rehypeKatex, rehypeRaw]}
       {...props}
     >
       {children}
