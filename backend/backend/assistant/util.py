eb948b904f07cc5e8063f49bba42c2977eb3dc66
Joong-Won Seo <joong.won.seo@gmail.com>
2024-01-20 01:39:01 +0100

    formatting

--- backend/backend/assistant/util.py
+++ backend/backend/assistant/util.py
@@ -1,12 +1,12 @@
 def format_query_result(query_result):
-    result = "\n".join(f"[^{i+1}]:{d.speaker}: *{d.content}*" for i, (score, d) in enumerate(query_result))
+    result = "\n".join(f"[^{i+1}]: **{d.speaker.strip()}:** *{d.content.strip()}*" for i, (score, d) in enumerate(query_result))
     if result:
         return result
     else:
         return "No result found"
 
 def format_query_result_law(query_result):
-    result = "\n".join(f"[^{i+1}]:{d.content}" for i, (score, d) in enumerate(query_result))
+    result = "\n".join(f"[^{i+1}]: *{d.content.strip()}*" for i, (score, d) in enumerate(query_result))
     if result:
         return result
     else:
