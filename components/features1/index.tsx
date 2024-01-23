f2fc335c36c94cb10e99ffa66671db2ad6120ac3
y-y-h-h <ge38xay@mytum.de>
2024-01-23 20:01:14 +0100

    landingpagechange 1

---
+++ components/features1/index.tsx
@@ -0,0 +1,126 @@
+import {Divider, Grid, Text} from '@nextui-org/react';
+import React from 'react';
+import {BoxIcon} from '../icons/BoxIcon';
+import {FeatureIcon} from '../icons/FeatureIcon';
+import {Box} from '../styles/box';
+import {Flex} from '../styles/flex';
+
+export const Features1 = () => {
+   return (
+      <>
+         <Flex
+            direction={'column'}
+            css={{
+               'gap': '1rem',
+               'pt': '$20',
+               'justifyContent': 'center',
+               'alignItems': 'center',
+               'px': '$6',
+               '@sm': {
+                  justifyContent: 'space-around',
+                  px: '$32',
+                  flexDirection: 'row',
+               },
+               '@md': {
+                  px: '$64',
+               },
+            }}
+         >
+            <Flex direction="column">
+               <Text span css={{color: '$blue600'}}>
+                  Show Case 
+               </Text>
+               <Text h3>Marco's Divorce Case</Text>
+               <Text
+                  span
+                  css={{
+                     maxWidth: '400px',
+                     color: '$accents8',
+                  }}
+               >
+                  Marco wants to find a lawyer to help him with his divorce issue. After spending hours searching for a lawyer, 
+                  he still could not find a suitable candidate. After hours of searching and waiting for other firms to reply, 
+                  he stubles upon a website that uses ChatJustus. 
+               </Text>
+
+               <Flex
+                  css={{
+                     py: '$10',
+                     gap: '$5',
+                  }}
+               >
+                  <BoxIcon />
+                  <Flex direction={'column'}>
+                     <Text h4 weight={'medium'}>
+                        Simple Client Lawyer Matching 
+                     </Text>
+                     <Text
+                        span
+                        css={{
+                           maxWidth: '400px',
+                           color: '$accents8',
+                        }}
+                     >
+                        text
+                     </Text>
+                  </Flex>
+               </Flex>
+               <Flex
+                  css={{
+                     py: '$10',
+                     gap: '$5',
+                  }}
+               >
+                  <BoxIcon />
+                  <Flex direction={'column'}>
+                     <Text h4 weight={'medium'}>
+                        Your title here
+                     </Text>
+                     <Text
+                        span
+                        css={{
+                           maxWidth: '400px',
+                           color: '$accents8',
+                        }}
+                     >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed condimentum, nisl ut aliquam lacinia, nisl nisl
+                        aliquet nisl,
+                     </Text>
+                  </Flex>
+               </Flex>
+               <Flex
+                  css={{
+                     py: '$10',
+                     gap: '$5',
+                  }}
+               >
+                  <BoxIcon />
+                  <Flex direction={'column'}>
+                     <Text h4 weight={'medium'}>
+                        Your title here
+                     </Text>
+                     <Text
+                        span
+                        css={{
+                           maxWidth: '400px',
+                           color: '$accents8',
+                        }}
+                     >
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed condimentum, nisl ut aliquam lacinia, nisl nisl
+                        aliquet nisl,
+                     </Text>
+                  </Flex>
+               </Flex>
+            </Flex>
+            <Flex align={'center'}>
+               <FeatureIcon />
+            </Flex>
+         </Flex>
+         <Divider
+            css={{position: 'absolute', inset: '0p', left: '0', mt: '$5'}}
+         />
+      </>
+   );
+};
---
+++ components/hero/index.tsx
@@ -0,0 +1,87 @@
+import {Button, Divider, Input, Text} from '@nextui-org/react';
+import React from 'react';
+import {CheckIcon} from '../icons/CheckIcon';
+import {Box} from '../styles/box';
+import {Flex} from '../styles/flex';
+
+export const Hero = () => {
+   return (
+      <>
+         <Flex
+            css={{
+               'gap': '$3',
+               'px': '$6',
+               'flexDirection': 'column',
+               'alignContent': 'center',
+               'justifyContent': 'center',
+               'alignItems': 'center',
+               'width': '100%',
+               '@sm': {
+                  flexDirection: 'row',
+                  mt: '$20',
+               },
+            }}
+            justify={'center'}
+         >
+            <Box
+               css={{
+                  pt: '$13',
+
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '$5',
+               }}
+            >
+               <Box
+                  css={{
+                     maxWidth: '600px',
+                  }}
+               >
+                  <Text
+                     h1
+                     css={{
+                        display: 'inline',
+                     }}
+                  >
+                     ChatJustus
+                  </Text>
+               </Box>
+
+               <Text
+                  css={{
+                     color: '$accents8',
+                     maxWidth: '400px',
+                  }}
+                  size={'$lg'}
+                  span
+               >
+                  WHERE INNOVATION MEETS SIMPLICITY.
+               </Text>
+
+               <Flex
+                  css={{
+                     gap: '$8',
+                     pt: '$4',
+                  }}
+                  wrap={'wrap'}
+               >
+                  <Button>Try Demo</Button>
+               </Flex>
+            </Box>
+            <Box
+               css={{
+                  '& img': {
+                     width: '575px',
+                     objectFit: 'contain',
+                  },
+               }}
+            >
+               <img src="mock.png" />
+            </Box>
+         </Flex>
+         <Divider
+            css={{position: 'absolute', inset: '0p', left: '0', mt: '$10'}}
+         />
+      </>
+   );
+};
